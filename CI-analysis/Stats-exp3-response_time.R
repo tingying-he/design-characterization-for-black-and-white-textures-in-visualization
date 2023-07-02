b9a4ad41b8fc7fd9cd9a80a216d4353df155a6e8
tingying_he <tingying.he@outlook.com>
2023-07-02 14:57:21 +0200

    supp materials

---
+++ CI-analysis/Stats-exp3-response_time.R
@@ -0,0 +1,156 @@
+library(plyr)
+library(dplyr)
+library(tidyr)
+
+source("CI-Functions-Bonferroni.R")
+
+
+mydata <- read.table("exp-data/response_time.csv", header=T, sep=",")
+filename_analysis <- "results/"
+
+##Need to refactor the data a bit
+
+#This was a between-subjects experiment
+bardf <- mydata %>% select(matches("bar|participant_id"))
+bardf <- bardf %>% drop_na(response_time_bar_geo)
+bardf <- bardf %>% 
+  rename(
+    geometric = response_time_bar_geo,
+    iconic = response_time_bar_icon,
+    unicolor = response_time_bar_color
+  )
+
+piedf <- mydata %>% select(matches("pie|participant_id"))
+piedf <- piedf %>% drop_na(response_time_pie_geo)
+piedf <- piedf %>% 
+  rename(
+    geometric = response_time_pie_geo,
+    iconic = response_time_pie_icon,
+    unicolor = response_time_pie_color
+  )
+
+
+##Calculating threshold CIs for the bar charts
+technique_bar_geo <- bootstrapMeanCI(bardf$geometric)
+technique_bar_icon <- bootstrapMeanCI(bardf$iconic)
+technique_bar_unicolor <- bootstrapMeanCI(bardf$unicolor)
+
+##Calculating threshold CIs for the pie charts
+technique_pie_geo <- bootstrapMeanCI(piedf$geometric)
+technique_pie_icon <- bootstrapMeanCI(piedf$iconic)
+technique_pie_unicolor <- bootstrapMeanCI(piedf$unicolor)
+
+
+###changing the data structure a bit
+#bar chart
+barData <- c()
+barData$name <- c("geometric","iconic","unicolor")
+barData$pointEstimate <- c(technique_bar_geo[1], technique_bar_icon[1],technique_bar_unicolor[1])
+barData$ci.max <- c(technique_bar_geo[3], technique_bar_icon[3],technique_bar_unicolor[3])
+barData$ci.min <- c(technique_bar_geo[2], technique_bar_icon[2],technique_bar_unicolor[2])
+
+#pie chart
+pieData <- c()
+pieData$name <- c("geometric","iconic","unicolor")
+pieData$pointEstimate <- c(technique_pie_geo[1], technique_pie_icon[1],technique_pie_unicolor[1])
+pieData$ci.max <- c(technique_pie_geo[3], technique_pie_icon[3],technique_pie_unicolor[3])
+pieData$ci.min <- c(technique_pie_geo[2], technique_pie_icon[2],technique_pie_unicolor[2])
+
+##preparting to print
+#bar chart
+#bardatatoprint <- data.frame(factor(barData$name),barData$pointEstimate, barData$ci.min, barData$ci.max)
+#colnames(bardatatoprint) <- c("technique", "time", "lowerBound_CI", "upperBound_CI ")
+
+#pie chart
+#piedatatoprint <- data.frame(factor(pieData$name),pieData$pointEstimate, pieData$ci.min, pieData$ci.max)
+#colnames(piedatatoprint) <- c("technique", "time", "lowerBound_CI", "upperBound_CI ")
+
+
+####plotting
+#bar chart
+bardatatoprint <- data.frame(factor(barData$name),barData$pointEstimate, barData$ci.min, barData$ci.max)
+colnames(bardatatoprint) <- c("technique", "mean_time", "lowerBound_CI", "upperBound_CI ")
+gbar <- barChart(bardatatoprint,barData$name ,nbTechs = 3, ymin = 0, ymax = 3000, gray_line_interval = 500, "", "","") #Avg. Thresholds. Error Bars, Bootstrap 95% CIs
+print(gbar)
+
+fname <- "results/exp3_response_time_bar.pdf"
+ggsave(filename = fname, plot=gbar, width = 8, height= 1, device= pdf(), units = "in")
+dev.off()
+
+tmp <- paste(filename_analysis, "exp3_response_time_bar.csv", collapse="_")
+write.csv(bardatatoprint,file=tmp)
+
+
+
+#pie chart
+piedatatoprint <- data.frame(factor(pieData$name),pieData$pointEstimate, pieData$ci.min, pieData$ci.max)
+colnames(piedatatoprint) <- c("technique", "mean_time", "lowerBound_CI", "upperBound_CI ")
+gpie <- barChart(piedatatoprint,pieData$name ,nbTechs = 3, ymin = 0, ymax = 3000, gray_line_interval = 500,"", "","") #Avg. Thresholds. Error Bars, Bootstrap 95% CIs
+print(gpie)
+fname <- "results/exp3_response_time_pie.pdf"
+ggsave(filename = fname, plot=gpie, width = 8, height= 1, device= pdf(), units = "in")
+dev.off()
+
+tmp <- paste(filename_analysis, "exp3_response_time_pie.csv", collapse="_")
+write.csv(piedatatoprint,file=tmp)
+
+########################
+
+# CIs with adapted alpha value for multiple comparisons --- calculate the differences between techniques for each chart separately
+diff_geo_icon_bar = bootstrapMeanCI_corr(bardf$geometric  - bardf$iconic, 3)
+diff_geo_uni_bar  = bootstrapMeanCI_corr(bardf$geometric  - bardf$unicolor, 3)
+diff_uni_icon_bar = bootstrapMeanCI_corr(bardf$unicolor - bardf$iconic, 3)
+
+diff_geo_icon_pie = bootstrapMeanCI_corr(piedf$geometric  - piedf$iconic, 3)
+diff_geo_uni_pie  = bootstrapMeanCI_corr(piedf$geometric  - piedf$unicolor, 3)
+diff_uni_icon_pie = bootstrapMeanCI_corr(piedf$unicolor - piedf$iconic, 3)
+
+
+analysisBar <- c()
+analysisBar$name <- c("geo-icon", "geo-uni", "uni-icon")
+analysisBar$pointEstimate <- c(diff_geo_icon_bar[1], diff_geo_uni_bar[1], diff_uni_icon_bar[1])
+analysisBar$ci.max <- c(diff_geo_icon_bar[3], diff_geo_uni_bar[3], diff_uni_icon_bar[3])
+analysisBar$ci.min <- c(diff_geo_icon_bar[2], diff_geo_uni_bar[2], diff_uni_icon_bar[2])
+analysisBar$level  <- c(diff_geo_icon_bar[4], diff_geo_uni_bar[4], diff_uni_icon_bar[4])
+analysisBar$ci_corr.max <- c(diff_geo_icon_bar[6], diff_geo_uni_bar[6], diff_uni_icon_bar[6])
+analysisBar$ci_corr.min <- c(diff_geo_icon_bar[5], diff_geo_uni_bar[5], diff_uni_icon_bar[5])
+
+analysisPie <- c()
+analysisPie$name <- c("geo-icon", "geo-uni", "uni-icon")
+analysisPie$pointEstimate <- c(diff_geo_icon_pie[1], diff_geo_uni_pie[1], diff_uni_icon_pie[1])
+analysisPie$ci.max <- c(diff_geo_icon_pie[3], diff_geo_uni_pie[3], diff_uni_icon_pie[3])
+analysisPie$ci.min <- c(diff_geo_icon_pie[2], diff_geo_uni_pie[2], diff_uni_icon_pie[2])
+analysisPie$level  <- c(diff_geo_icon_pie[4], diff_geo_uni_pie[4], diff_uni_icon_pie[4])
+analysisPie$ci_corr.max <- c(diff_geo_icon_pie[6], diff_geo_uni_pie[6], diff_uni_icon_pie[6])
+analysisPie$ci_corr.min <- c(diff_geo_icon_pie[5], diff_geo_uni_pie[5], diff_uni_icon_pie[5])
+
+
+###Plotting the differences
+
+#Bar chart
+datatoprintbar <- data.frame(factor(analysisBar$name), analysisBar$pointEstimate, analysisBar$ci.min, analysisBar$ci.max, analysisBar$level, analysisBar$ci_corr.min, analysisBar$ci_corr.max)
+colnames(datatoprintbar) <- c("technique", "mean_time", "lowerBound_CI", "upperBound_CI", "corrected_CI", "lowerBound_CI_corr", "upperBound_CI_corr") #We use the name mean_time for the value of the mean even though it's not a time, it's just to parse the data for the plot
+g <- barChart_corr(datatoprintbar,analysisBar$name ,nbTechs = 3, ymin = -400, ymax = 400,  gray_line_interval = 100,"", "","") #Avg. Thresholds. Error Bars, Bootstrap 95% CIs
+print(g)
+
+fname <- "results/exp3_response_time_diff_bar.pdf"
+ggsave(filename = fname, plot=g, width = 8, height= 1, device= pdf(), units = "in")
+dev.off()
+
+tmp <- paste(filename_analysis, "exp3_response_time_diff_bar.csv", collapse="_")
+write.csv(datatoprintbar,file=tmp)
+
+#Pie chart
+datatoprintpie <- data.frame(factor(analysisPie$name), analysisPie$pointEstimate, analysisPie$ci.min, analysisPie$ci.max, analysisPie$level, analysisPie$ci_corr.min, analysisPie$ci_corr.max)
+colnames(datatoprintpie) <- c("technique", "mean_time", "lowerBound_CI", "upperBound_CI", "corrected_CI", "lowerBound_CI_corr", "upperBound_CI_corr") #We use the name mean_time for the value of the mean even though it's not a time, it's just to parse the data for the plot
+g <- barChart_corr(datatoprintpie,analysisPie$name ,nbTechs = 3, ymin = -400, ymax = 400,  gray_line_interval = 100, "", "","") #Avg. Thresholds. Error Bars, Bootstrap 95% CIs
+print(g)
+
+fname <- "results/exp3_response_time_diff_pie.pdf"
+ggsave(filename = fname, plot=g, width = 8, height= 1, device= pdf(), units = "in")
+dev.off()
+
+tmp <- paste(filename_analysis, "exp3_response_time_diff_pie.csv", collapse="_")
+write.csv(datatoprintpie,file=tmp)
+
+
---
+++ CI-analysis/Stats-map.R
@@ -0,0 +1,95 @@
+library(plyr)
+library(dplyr)
+library(tidyr)
+
+source("CI-Functions-Bonferroni.R")
+mydata <- read.table("exp2_map.csv", header=T, sep=",")
+filename_analysis <- "results/"
+
+##Need to refactor the data a bit
+
+#This was a between-subjects experiment
+mapdf <- mydata %>% select(matches("map|participant_id"))
+mapdf <- mapdf %>% drop_na(beauvis_map_geo)
+mapdf <- mapdf %>% 
+  rename(
+    geometric = beauvis_map_geo,
+    iconic = beauvis_map_icon,
+    #unicolor = beauvis_map_color
+  )
+
+
+##Calculating threshold CIs for the map charts
+technique_map_geo <- bootstrapMeanCI(mapdf$geometric)
+technique_map_icon <- bootstrapMeanCI(mapdf$iconic)
+#technique_map_unicolor <- bootstrapMeanCI(mapdf$unicolor)
+
+
+###changing the data structure a bit
+#map chart
+mapData <- c()
+mapData$name <- c("geometric","iconic")
+mapData$pointEstimate <- c(technique_map_geo[1], technique_map_icon[1])
+mapData$ci.max <- c(technique_map_geo[3], technique_map_icon[3])
+mapData$ci.min <- c(technique_map_geo[2], technique_map_icon[2])
+
+
+##preparting to print
+#map chart
+#mapdatatoprint <- data.frame(factor(mapData$name),mapData$pointEstimate, mapData$ci.min, mapData$ci.max)
+#colnames(mapdatatoprint) <- c("technique", "time", "lowerBound_CI", "upperBound_CI ")
+
+#map chart
+#mapdatatoprint <- data.frame(factor(mapData$name),mapData$pointEstimate, mapData$ci.min, mapData$ci.max)
+#colnames(mapdatatoprint) <- c("technique", "time", "lowerBound_CI", "upperBound_CI ")
+
+
+####plotting
+#map chart
+mapdatatoprint <- data.frame(factor(mapData$name),mapData$pointEstimate, mapData$ci.min, mapData$ci.max)
+colnames(mapdatatoprint) <- c("technique", "mean_time", "lowerBound_CI", "upperBound_CI ")
+gmap <- barChart(mapdatatoprint,mapData$name ,nbTechs = 2, ymin = 1, ymax = 7, gray_line_interval = 1, "", "","map") #Avg. Thresholds. Error maps, Bootstrap 95% CIs
+print(gmap)
+
+fname <- "results/exp2_beauvis_map.pdf"
+ggsave(filename = fname, plot=gmap, width = 8, height= 0.8, device= pdf(), units = "in")
+dev.off()
+
+tmp <- paste(filename_analysis, "exp2_beauvis_map.csv", collapse="_")
+write.csv(mapdatatoprint,file=tmp)
+
+
+########################
+
+# CIs with adapted alpha value for multiple comparisons --- calculate the differences between techniques for each chart separately
+diff_geo_icon_map = bootstrapMeanCI_corr(mapdf$geometric  - mapdf$iconic, 1)
+#diff_geo_uni_map  = bootstrapMeanCI_corr(mapdf$geometric  - mapdf$unicolor, 3)
+#diff_uni_icon_map = bootstrapMeanCI_corr(mapdf$unicolor - mapdf$iconic, 3)
+
+
+analysisMap <- c()
+#analysisMap$name <- c("geo-icon", "geo-uni", "uni-icon")
+analysisMap$name <- c("geo-icon")
+analysisMap$pointEstimate <- c(diff_geo_icon_map[1])
+analysisMap$ci.max <- c(diff_geo_icon_map[3])
+analysisMap$ci.min <- c(diff_geo_icon_map[2])
+analysisMap$level  <- c(diff_geo_icon_map[4])
+analysisMap$ci_corr.max <- c(diff_geo_icon_map[6])
+analysisMap$ci_corr.min <- c(diff_geo_icon_map[5])
+
+
+###Plotting the differences
+
+#Map chart
+datatoprintmap <- data.frame(factor(analysisMap$name), analysisMap$pointEstimate, analysisMap$ci.min, analysisMap$ci.max, analysisMap$level, analysisMap$ci_corr.min, analysisMap$ci_corr.max)
+colnames(datatoprintmap) <- c("technique", "mean_time", "lowerBound_CI", "upperBound_CI", "corrected_CI", "lowerBound_CI_corr", "upperBound_CI_corr") #We use the name mean_time for the value of the mean even though it's not a time, it's just to parse the data for the plot
+g <- barChart_corr(datatoprintmap,analysisMap$name ,nbTechs = 1, ymin = -3, ymax = 3, gray_line_interval = 1, "", "","map") #Avg. Thresholds. Error Bars, Bootstrap 95% CIs
+print(g)
+
+fname <- "results/exp2_beauvis_diff_map.pdf"
+ggsave(filename = fname, plot=g, width = 8, height= 0.6, device= pdf(), units = "in")
+dev.off()
+
+tmp <- paste(filename_analysis, "exp2_beauvis_diff_map.csv", collapse="_")
+write.csv(datatoprintmap,file=tmp)
+
